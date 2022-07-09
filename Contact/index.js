99774b7c96188ce55e364e73471c6e4bb74982b7
nhat2012782 <gonbbb1@gmail.com>
2022-07-09 22:39:19 +0700

    Thêm form vào mục liên hệ

---
+++ Contact/index.js
@@ -0,0 +1,7 @@
+const imgSrc = "/assets/images/Timetable/grade6.jpg";
+
+const contentInner = $(".content__inner");
+
+contentInner.innerHTML = `
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScJ10PyyvDtGTb-90zVbqd3_8Bj4uj6PeTXt6GXoR2l7r4A1g/viewform?embedded=true" width="640" height="1082" frameborder="0" marginheight="0" marginwidth="0">Đang tải…</iframe>
+`;
--- HomePage/index.js
+++ HomePage/index.js
@@ -7,95 +7,110 @@ const indexItems = $$(".index__item");
 const sliderLeftArrow = $(".slider .arrow-left__icon");
 const sliderRightArrow = $(".slider .arrow-right__icon");
 
-let slideHandler = setInterval(() => {
-  let index = Array.from(sliderImgs).findIndex((item) =>
-    item.classList.contains("active")
-  );
-  sliderImgs[index].classList.remove("active");
-  indexItems[index].classList.remove("active");
+function start() {
+  function handleSlider() {
+    let slideHandler = setInterval(() => {
+      let index = Array.from(sliderImgs).findIndex((item) =>
+        item.classList.contains("active")
+      );
+      sliderImgs[index].classList.remove("active");
+      indexItems[index].classList.remove("active");
+    
+      if (index === 2) {
+        index = 0;
+      } else {
+        index++;
+      }
+    
+      sliderImgs[index].classList.add("active");
+      indexItems[index].classList.add("active");
+    }, 5000);
+    
+    function resetSlideHandler() {
+      clearInterval(slideHandler);
+      slideHandler = setInterval(() => {
+        let index = Array.from(sliderImgs).findIndex((item) =>
+          item.classList.contains("active")
+        );
+        sliderImgs[index].classList.remove("active");
+        indexItems[index].classList.remove("active");
+    
+        if (index === 2) {
+          index = 0;
+        } else {
+          index++;
+        }
+    
+        sliderImgs[index].classList.add("active");
+        indexItems[index].classList.add("active");
+      }, 5000);
+    }
+    
+    Array.from(indexItems).forEach((item, index) => {
+      item.onclick = function () {
+        $(".slider-img__item.active").classList.remove("active");
+        $(".index__item.active").classList.remove("active");
+        sliderImgs[index].classList.add("active");
+        indexItems[index].classList.add("active");
+        resetSlideHandler();
+      };
+    });
+    
+    sliderLeftArrow.onclick = () => {
+      let index = Array.from(sliderImgs).findIndex((item) =>
+        item.classList.contains("active")
+      );
+      sliderImgs[index].classList.remove("active");
+      indexItems[index].classList.remove("active");
+    
+      if (index === 0) {
+        index = 2;
+      } else {
+        index--;
+      }
+    
+      sliderImgs[index].classList.add("active");
+      indexItems[index].classList.add("active");
+      resetSlideHandler();
+    };
+    
+    sliderRightArrow.onclick = () => {
+      let index = Array.from(sliderImgs).findIndex((item) =>
+        item.classList.contains("active")
+      );
+      sliderImgs[index].classList.remove("active");
+      indexItems[index].classList.remove("active");
+    
+      if (index === 2) {
+        index = 0;
+      } else {
+        index++;
+      }
+    
+      sliderImgs[index].classList.add("active");
+      indexItems[index].classList.add("active");
+      resetSlideHandler();
+    };
+  }
+  
+  function handleLogo() {
+    const logoSlider = $(".desti_logo__list");
+    const logoItem = logoSlider.querySelector('li')
+    // desti Slider;
 
-  if (index === 2) {
-    index = 0;
-  } else {
-    index++;
+    const handleLogoSlider = setInterval(() => {
+      if (logoItem.clientWidth * logoSlider.childElementCount - logoSlider.scrollLeft >= 0) {
+        logoSlider.scrollLeft += 1;
+      } else clearInterval(handleLogoSlider);
+    }, 10);
   }
 
-  sliderImgs[index].classList.add("active");
-  indexItems[index].classList.add("active");
-}, 5000);
-
-function resetSlideHandler() {
-  clearInterval(slideHandler);
-  slideHandler = setInterval(() => {
-    let index = Array.from(sliderImgs).findIndex((item) =>
-      item.classList.contains("active")
-    );
-    sliderImgs[index].classList.remove("active");
-    indexItems[index].classList.remove("active");
-
-    if (index === 2) {
-      index = 0;
-    } else {
-      index++;
-    }
-
-    sliderImgs[index].classList.add("active");
-    indexItems[index].classList.add("active");
-  }, 5000);
+  handleSlider()
+  handleLogo()
 }
 
-Array.from(indexItems).forEach((item, index) => {
-  item.onclick = function () {
-    $(".slider-img__item.active").classList.remove("active");
-    $(".index__item.active").classList.remove("active");
-    sliderImgs[index].classList.add("active");
-    indexItems[index].classList.add("active");
-    resetSlideHandler();
-  };
-});
+start()
 
-sliderLeftArrow.onclick = () => {
-  let index = Array.from(sliderImgs).findIndex((item) =>
-    item.classList.contains("active")
-  );
-  sliderImgs[index].classList.remove("active");
-  indexItems[index].classList.remove("active");
 
-  if (index === 0) {
-    index = 2;
-  } else {
-    index--;
-  }
 
-  sliderImgs[index].classList.add("active");
-  indexItems[index].classList.add("active");
-  resetSlideHandler();
-};
 
-sliderRightArrow.onclick = () => {
-  let index = Array.from(sliderImgs).findIndex((item) =>
-    item.classList.contains("active")
-  );
-  sliderImgs[index].classList.remove("active");
-  indexItems[index].classList.remove("active");
-
-  if (index === 2) {
-    index = 0;
-  } else {
-    index++;
-  }
-
-  sliderImgs[index].classList.add("active");
-  indexItems[index].classList.add("active");
-  resetSlideHandler();
-};
-
-const logoSlider = $(".desti_logo__list");
-
-// desti Slider
-
-const handleLogoSlider = setInterval(() => {
-  if (logoSlider.offsetWidth - logoSlider.scrollLeft >= 5) {
-    logoSlider.scrollLeft += 1;
-  } else clearInterval(handleLogoSlider);
-}, 10);
